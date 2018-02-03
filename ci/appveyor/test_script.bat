2371009ccc32fedbe22dc1acee72274c9cf770a4
Antoine <end2endzone@gmail.com>
2018-02-03 10:33:18 -0500

    Renamed testWin32Arduino to win32Arduino_unittest.

--- ci/appveyor/test_script.bat
+++ ci/appveyor/test_script.bat
@@ -7,7 +7,7 @@ cd ..\..\src\bin\Win32\Release
 echo =======================================================================
 echo Running unit tests...
 echo =======================================================================
-testWin32Arduino.exe
+win32Arduino_unittest.exe
 echo done
 echo.
 
@@ -15,7 +15,7 @@ echo =======================================================================
 echo Uploading test results to AppVeyor
 echo =======================================================================
 set TEST_RESULT_URL=https://ci.appveyor.com/api/testresults/junit/%APPVEYOR_JOB_ID%
-set TEST_RESULT_FILE=%CD%\testWin32Arduino.testResults.xml
+set TEST_RESULT_FILE=%CD%\win32Arduino_unittest.testResults.xml
 echo TEST_RESULT_URL=%TEST_RESULT_URL%
 echo TEST_RESULT_FILE=%TEST_RESULT_FILE%
 powershell "(New-Object 'System.Net.WebClient').UploadFile($($env:TEST_RESULT_URL), $($env:TEST_RESULT_FILE))"
