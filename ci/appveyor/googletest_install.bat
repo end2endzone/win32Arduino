0baf852e064e51f92d95041064d5b73269011327
Antoine <end2endzone@gmail.com>
2018-03-24 14:19:01 -0400

    Cleaned up googletest_install.bat script.

--- ci/appveyor/googletest_install.bat
+++ ci/appveyor/googletest_install.bat
@@ -1,16 +1,15 @@
 @echo off
 
 echo =======================================================================
-echo Finding root folder of repository
+echo Setting environnment variables for googletest
 echo =======================================================================
 cd /d %~dp0
 cd ..\..
-set REPOSITORY_ROOT=%cd%
-echo REPOSITORY_ROOT=%REPOSITORY_ROOT%
-echo done.
+set  GOOGLETEST_HOME=%REPOSITORY_ROOT%\third_party\googletest
+setx GOOGLETEST_HOME %GOOGLETEST_HOME%
+echo Found GOOGLETEST_HOME=%GOOGLETEST_HOME%
 echo.
 
-set GOOGLETEST_HOME=%REPOSITORY_ROOT%\third_party\googletest
 echo Downloading googletest to folder %GOOGLETEST_HOME%
 echo.
 
@@ -49,23 +48,6 @@ echo.
 
 echo Launching cmake...
 cd /d %GOOGLETEST_HOME%\build
-cmake -G "Visual Studio 10 2010" -Dgtest_force_shared_crt=ON -DCMAKE_CXX_FLAGS_DEBUG=/MDd -DCMAKE_CXX_FLAGS_RELEASE=/MD "%GOOGLETEST_HOME%"
-echo done.
-echo.
-
-echo =======================================================================
-echo Setting environnment variables for googletest
-echo =======================================================================
-set GTEST_DEBUG_LIBRARIES=gtest.lib
-set GTEST_RELEASE_LIBRARIES=gtest.lib
-set GTEST_INCLUDE=%GOOGLETEST_HOME%\include
-set GTEST_LIBRARY_DIR=%GOOGLETEST_HOME%\build
-REM
-setx GTEST_DEBUG_LIBRARIES %GTEST_DEBUG_LIBRARIES%
-setx GTEST_RELEASE_LIBRARIES %GTEST_RELEASE_LIBRARIES%
-setx GTEST_INCLUDE %GTEST_INCLUDE%
-setx GTEST_LIBRARY_DIR %GTEST_LIBRARY_DIR%
-REM
-set GTEST_
+cmake -G "Visual Studio 10 2010" -Dgtest_force_shared_crt=ON -DCMAKE_CXX_FLAGS_DEBUG=/MDd -DCMAKE_CXX_FLAGS_RELEASE=/MD ..
 echo done.
 echo.
