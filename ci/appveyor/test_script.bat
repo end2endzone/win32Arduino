9b1fd7446d9a3d50c9f0f9600e63728ff9fbcddb
Antoine <end2endzone@gmail.com>
2018-04-12 21:28:22 -0400

    Fixed appveyor test_script.bat to call ant `test` target and then upload test result. Required for issue #15 - win32Arduino on Linux

--- ci/appveyor/test_script.bat
+++ ci/appveyor/test_script.bat
@@ -2,18 +2,20 @@
 
 echo Change directory to output directory
 cd /d %~dp0
-cd ..\..\cmake\build\bin\Release
+cd ..\ant
 
 echo =======================================================================
 echo Running unit tests...
 echo =======================================================================
-win32Arduino_unittest.exe
+call ant -f build.xml test
 echo done
 echo.
 
 echo =======================================================================
 echo Uploading test results to AppVeyor
 echo =======================================================================
+cd /d %~dp0
+cd ..\..\publish
 set TEST_RESULT_URL=https://ci.appveyor.com/api/testresults/junit/%APPVEYOR_JOB_ID%
 set TEST_RESULT_FILE=%CD%\win32Arduino_unittest.x86.release.xml
 echo TEST_RESULT_URL=%TEST_RESULT_URL%
